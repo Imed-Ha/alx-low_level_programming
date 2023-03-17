9e446e92b8f61d5777b2aa2debd3779e9a0089a7
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-17 15:50:40 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,13 @@
  */
 int main(void)
 {
-int i;
-for (i = 0; i < 10; i++)
-{
- putchar(i + '0');
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	putchar('\n');
+
+	return (0);
 }
