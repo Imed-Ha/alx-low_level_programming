63d4772c8fefb5cb3bcde6fe1f3254adbfcb1a2a
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-17 15:22:22 -0700

    4-print_alphabat.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,24 +3,20 @@
 /**
  * main - Entry point
  *
- * Description: Prints the alphabet in lowercase, except 'e' and 'q'
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-    char c;
+	char letter;
 
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        if (c != 'e' && c != 'q')
-        {
-            putchar(c);
-        }
-    }
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
 
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
