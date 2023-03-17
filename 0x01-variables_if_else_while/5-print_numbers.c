3905083d4f62098a11be6153559b6d9421e19ba1
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-17 15:38:26 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,16 +2,19 @@
 
 /**
  * main - Entry point
- *
+ * Description: Prints all single digit numbers of base 10 starting from 0
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-        int i;
+    int num;
 
-        for (i = 0; i < 10; i++)
-                putchar(i + '0');
-        putchar('\n');
+    for (num = 0; num < 10; num++)
+    {
+        printf("%d", num);
+    }
+    printf("\n");
 
-        return (0);
+    return (0);
 }
