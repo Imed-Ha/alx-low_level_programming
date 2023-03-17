332498531ca0ed6354234839216f79cf5a0d8b57
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-17 15:13:09 -0700

    4-print_alphabat.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++)
+    {
+        if (c != 'e' && c != 'q')
+            putchar(c);
+    }
+
+    putchar('\n');
+
+    return 0;
+}
