a71478e181b56c0998e90350d9587a2fc747cfe7
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-21 16:09:17 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,5 @@
 #include "main.h"
-
-/**
- * print_alphabet - prints the alphabet in lowercase
- *
- * Return: void
- */
 void print_alphabet(void)
 {
-	char letter = 'a';
-
-	while (letter <= 'z')
-	{
-		_putchar(letter);
-	       	letter++;
-	}
-
-	_putchar('\n');
+    /* Code to print the alphabet */
 }
