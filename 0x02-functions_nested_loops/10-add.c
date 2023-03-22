4f4e23d2e1990a3aeeebc2f84b947edd1d854c22
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-22 16:11:55 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+/**
+ * add - adds two integers and returns the result
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: the result of the addition
+ */
+int add(int a, int b)
+{
+	return a + b;
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);
 void jack_bauer(void);
 void times_table(void);
+int add(int a, int b);
 int _isalpha(int c);
 int _islower(int c);
 int print_sign(int n);
