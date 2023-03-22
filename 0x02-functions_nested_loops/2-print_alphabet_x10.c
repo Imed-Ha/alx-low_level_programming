d7f3f0d497b462a547b31ec79a236de4d73c17d6
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-22 14:40:51 -0700

    2-alphabet_x10 3 x exit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+	_putchar('\n');
+	}
+}
