f776f20db4dd77d79f4d79ca93ad96161d60d34c
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-22 15:19:35 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - checks for alphabetic character.
  * @c: the character to be checked.
- * 
+ *
  * Return: 1 if c is a letter, lowercase or uppercase; 0 otherwise.
  */
 int _isalpha(int c)
