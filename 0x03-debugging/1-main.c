ea915c6f55f810634a5eefe60d0b7f3c318aa0fe
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-23 06:21:09 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,13 +13,10 @@ int main(void)
 
         i = 0;
 
-        /* Commenting out the infinite loop */
-        /*
         while (i < 10)
         {
                 putchar(i);
         }
-        */
 
         printf("Infinite loop avoided! \\o/\n");
 
