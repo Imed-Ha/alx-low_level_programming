bedda23edee60b31e41cdc97b695eacd4e2415d8
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-23 16:57:48 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
+int largest_number(int a, int b, int c);
 void positive_or_negative(int i);
 int main(void);
-int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
