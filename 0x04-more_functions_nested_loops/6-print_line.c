9d51a11684b3ab9d62c619038a0f6a37d50c0c15
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-24 02:59:17 -0700

    6-lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a line n times using the _putchar function
+ * @n: Number of times the character _ should be printed
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+ 		_putchar('_');
+	}
+
+	_putchar('\n');
+}
