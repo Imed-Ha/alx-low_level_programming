74b24df35644636ff0c9b43e8ba7a628899f48e2
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-24 03:15:02 -0700

    6-lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,25 +1,20 @@
 #include "main.h"
 
 /**
- * print_line - Prints a line n times using the _putchar function
- * @n: Number of times the character _ should be printed
+ * print_line - prints a straight line in the terminal
+ * @n: number of times the character _ should be printed
+ *
+ * Return: void
  */
 
 void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-		return;
+		for (i = 0; i < n; i++)
+			_putchar('_');
 	}
-
-	for (i = 0; i < n; i++)
-	{
- 		_putchar('_');
-	}
-
 	_putchar('\n');
 }
-
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_line(int n);
 int _isdigit(int c);
 int _putchar(char c);
 int _isupper(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
 #endif /* MAIN_H */
