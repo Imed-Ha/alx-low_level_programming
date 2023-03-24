67d98a32393b23c8aa0fae069f3ffd439f4792a3
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-23 18:17:53 -0700

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,14 @@
  */
 void print_most_numbers(void)
 {
-    int num;
+	int num;
 
-    for (num = 0; num <= 9; num++)
-    {
-        if (num != 2 && num != 4)
-        {
-            _putchar(num + '0');
-        }
-    }
-    _putchar('\n');
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
+	}
+	_putchar('\n');
 }
