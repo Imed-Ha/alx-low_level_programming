6622081d4ffc2f8c07d3fe4c90c0fae5afb802de
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-23 17:49:24 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: the first integer to multiply
+ * @b: the second integer to multiply
+ *
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,4 +3,5 @@
 int _isdigit(int c);
 int _putchar(char c);
 int _isupper(int c);
+int mul(int a, int b);
 #endif /* MAIN_H */
