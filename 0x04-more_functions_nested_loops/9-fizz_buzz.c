63b4a87cbebb4fdda2aadfbad3bee52b3db43196
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-24 06:13:14 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,20 +8,20 @@
 
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-            printf("FizzBuzz ");
-        else if (i % 3 == 0)
-            printf("Fizz ");
-        else if (i % 5 == 0)
-            printf("Buzz ");
-        else
-            printf("%d ", i);
-    }
-    printf("\n");
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
 
-    return (0);
+	return (0);
 }
