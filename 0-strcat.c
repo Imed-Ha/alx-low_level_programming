33026191319007d0385314c3a7f0a759c18d45fb
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-03-29 20:47:38 -0700

    0-strcat

---
+++ 0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+        char *ptr = dest;
+
+        /* Move pointer to the end of dest string */
+        while (*ptr != '\0')
+                ptr++;
+
+        /* Append src string to dest string */
+        while (*src != '\0')
+        {
+                *ptr = *src;
+                ptr++;
+                src++;
+        }
+
+        /* Add null byte at the end of dest string */
+        *ptr = '\0';
+
+        return (dest);
+}
