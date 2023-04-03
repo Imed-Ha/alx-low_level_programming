0df83c46d8e81cf8ebb7dc67e28b9420bba93a51
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-04-03 15:07:30 -0700

    1-memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ *
+ * @dest: pointer to destination buffer
+ * @src: pointer to source buffer
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to destination buffer
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return dest;
+}
