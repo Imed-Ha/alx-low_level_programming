41c05862396f631cc4b57ebe1eb72e53a2a02af2
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-04-03 14:39:52 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-    {
-        s[i] = b;
-    }
+	for (i = 0; i < n; i++)
+	{
+	    s[i] = b;
+	}
 
-    return s;
+	return s;
 }
