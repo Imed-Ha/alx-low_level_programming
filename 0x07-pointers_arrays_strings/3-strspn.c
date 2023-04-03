55a7b24c7bf4f1b5ff467eac789872f124575180
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-04-03 15:46:48 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * @s: string to check
  * @accept: string to match
  *
- * Return: number of bytes in the initial segment of s which consist only of bytes from accept
+ * Return: the initial segment of s which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
 {
