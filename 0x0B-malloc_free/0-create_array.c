8aad199ab2cb56eae612bde6fa6d57f2d088b14b
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-04-11 15:21:46 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+		return NULL;
+
+	arr = malloc(size * sizeof(char));
+	if (arr == NULL)
+		return NULL;
+
+	for (i = 0; i < size; i++)
+		arr[i] = c;
+
+	return arr;
+}
