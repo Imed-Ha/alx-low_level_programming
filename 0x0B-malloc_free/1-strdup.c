ce29251cc460ab376458530f03a5e665b3823434
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-04-11 15:38:08 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -13,7 +13,7 @@ char *_strdup(char *str)
 {
 	char *dup_str;
 	unsigned int i, len = 0;
-	
+
 	if (str == NULL)
 		return (NULL);
 
@@ -25,7 +25,7 @@ char *_strdup(char *str)
 	if (dup_str == NULL)
 		return (NULL);
 
-	for ( i = 0; i <= len; i++)
+	for (i = 0; i <= len; i++)
 		dup_str[i] = str[i];
 
 	return (dup_str);
