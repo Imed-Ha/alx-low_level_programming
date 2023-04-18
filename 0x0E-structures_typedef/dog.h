25753a413a6fce594f95a4f5bee185e1a54f06a8
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-04-17 18:45:49 -0700

    3-main.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,9 +11,9 @@
  */
 typedef struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 
 void init_dog(dog_t *d, char *name, float age, char *owner);
