1ee7d98b47e14848f7d3f6020e0c094e9f2b66b9
imed-ha <imed-eddine.haouli@univ-annaba.org>
2023-05-02 10:40:08 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
+ * print_listint - elements of a listint_t list.
  * @h: A pointer to the head of the list.
  *
  * Return: The number of nodes in the list.
